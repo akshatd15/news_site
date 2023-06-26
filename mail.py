f3b59f3b6142d99715137ab93f7f2674747aa816
DEVINx34 <devinx34@gmail.com>
2023-06-26 22:42:38 +0530

    added topic for news , sending link of the news

--- mail.py
+++ mail.py
@@ -8,7 +8,7 @@ def send_mail(message):
 
     username = "devinx34@gmail.com"
     password = os.getenv("PASSWORD")
-    receiver = "devinx34@gmail.com"
+    receiver = "baliyanakshat@gmail.com"
 
     context = ssl.create_default_context()
 
--- main.py
+++ main.py
@@ -2,9 +2,13 @@ import requests
 import mail
 
 api_key = "a1a8fc4bb8034d5788694eb91aa02692"
-
-url = "https://newsapi.org/v2/everything?q=tesla&from=2023-05-2" \
-          "6&sortBy=publishedAt&apiKey=a1a8fc4bb8034d5788694eb91aa02692"
+topic = "tesla"
+url = "https://newsapi.org/v2/everything?" \
+      f"q={topic}&" \
+      "from=2023-05-26&" \
+      "sortBy=publishedAt&" \
+      "apiKey=a1a8fc4bb8034d5788694eb91aa02692&" \
+      "language=en"
 
 # Make request
 r = requests.get(url)
@@ -14,9 +18,14 @@ content = r.json()
 
 # access the article, title and description
 body = ""
-for article in content["articles"]:
+for article in content["articles"][:20]:
     if article["description"] is not None:
-        body = body + article["title"] + "\n" + article["description"] + 2*"\n"
+        body = body + article["title"] + "\n" + article["description"] + "\n" + \
+               article["url"] + 2 * "\n"
 
-body = body.encode("utf-8")
-mail.send_mail(message=body)
+msg = f"""\
+Subject: Today's News
+{body}"""
+
+msg = msg.encode("utf-8")
+mail.send_mail(message=msg)
