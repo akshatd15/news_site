0efd4b4891ba2c847ffc59df2f29770117a10121
DEVINx34 <devinx34@gmail.com>
2023-06-26 18:43:53 +0530

    send mail

---
+++ mail.py
@@ -0,0 +1,20 @@
+import smtplib, ssl
+import os
+
+
+def send_mail(message):
+    host = "smtp.gmail.com"
+    port = 465
+
+    username = "devinx34@gmail.com"
+    password = os.getenv("PASSWORD")
+    receiver = "devinx34@gmail.com"
+
+    context = ssl.create_default_context()
+
+    with smtplib.SMTP_SSL(host, port, context=context) as server:
+        server.login(username, password)
+        server.sendmail(username, receiver, message)
+
+
+
--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import requests
+import mail
 
 api_key = "a1a8fc4bb8034d5788694eb91aa02692"
 
 url = "https://newsapi.org/v2/everything?q=tesla&from=2023-05-2" \
-      "6&sortBy=publishedAt&apiKey=a1a8fc4bb8034d5788694eb91aa02692"
+          "6&sortBy=publishedAt&apiKey=a1a8fc4bb8034d5788694eb91aa02692"
 
 # Make request
 r = requests.get(url)
@@ -12,6 +13,10 @@ r = requests.get(url)
 content = r.json()
 
 # access the article, title and description
+body = ""
 for article in content["articles"]:
-    print(article["title"])
-    print(article["description"])
+    if article["description"] is not None:
+        body = body + article["title"] + "\n" + article["description"] + 2*"\n"
+
+body = body.encode("utf-8")
+mail.send_mail(message=body)
